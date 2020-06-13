85404b931ba8f7a6d515ae8f962fd03698391583
M. Foley <mjfoley@uwaterloo.ca>
2020-06-12 20:44:38 -0400

    whoops again

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
     error.httpStatusCode = 400
     return next(error)
   }
-  res.redirect("//speechvideos/successPage")
+  res.redirect("/speechvideos/successPage")
   
 })
  
